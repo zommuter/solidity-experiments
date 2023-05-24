ab9d4d4bf8c0b736d4d46293e84b028c7a9bcb62
Tobias Kienzler <zommuter@users.noreply.github.com>
2023-05-24 09:46:27 +0200

    added unsafeDeploy and salvage functions

--- contracts/create2experiments/Create3Factory.sol
+++ contracts/create2experiments/Create3Factory.sol
@@ -7,20 +7,39 @@ import "@0xsequence/create3/contracts/Create3.sol";
 
 /**
   @title A contract factory based on Agustin Aguilar's CREATE3 implementation https://github.com/0xsequence/create3
-  @notice The salt is re-hashed together with either the sender address `msg.sender` (using `privateDeploy`).
-          This serves the purpose of avoiding front-running a contract deployment to a given address.
-  @author Zommuter <zommuter+eth@gmail.com>
+  @notice The salt is re-hashed together with the sender address `msg.sender` (using `privateDeploy`), which serves
+          the purpose of avoiding front-running a contract deployment to a given address. The contract accepts ETH
+          payments as donations ;)
+          Alternatively to `privateDeploy`, a "salt" of zero can be used (via `unsafeDeploy`) independently of `msg.sender` at your own peril.
+  @author Zommuter <zommuter+eth@gmail.com> / https://twitter.com/zommuter_eth
 */
 contract Create3Factory {
+    // hard-coded to avoid front-runners exploiting owner = msg.sender, since this contract is to be deterministically deployed via
+    // https://github.com/Arachnid/deterministic-deployment-proxy / https://blockscan.com/address/0x4e59b44847b379578588920ca78fbf26c0b4956c
+    address payable constant zommuter = payable(0x4d25F3E88C10cDC001447beaEcA6e9Ee0009c060);
+
+    event DeployedPrivate(address);
+    event DeployedUnsafe(address);
+    event EmergencyCall(address, bool, bytes);
+    event EmergencyDelegateCall(address, bool, bytes);
+
     /**
     @notice Creates a new contract with given `_creationCode` and `_salt`, using the sender's address in the salt to avoid front-running
+            Usage is free, but a payment can be included as donation.
     @param _salt Salt of the contract creation, resulting address will be derived from this value
            only -- note the salt is salted with `msg.sender` in order to allow for front-running resistant deployment
     @param _creationCode Creation code (constructor) of the contract to be deployed, this value doesn't affect the resulting address
     @return addr of the deployed contract, reverts on error
     */
     function privateDeploy(bytes32 _salt, bytes calldata _creationCode) external payable returns (address) {
-        _salt = keccak256(bytes.concat(_salt, bytes20(msg.sender)));
+        address addr = _deploy(_salt, bytes20(msg.sender), _creationCode);
+        emit DeployedPrivate(addr);
+        return addr;
+    }
+
+    function _deploy(bytes32 _salt, bytes32 _salter, bytes calldata _creationCode) internal returns (address) {
+        zommuter.transfer(address(this).balance);
+        _salt = keccak256(bytes.concat(_salt, _salter));
         return Create3.create3(_salt, _creationCode);
     }
 
@@ -31,7 +50,7 @@ contract Create3Factory {
     @return addr of the deployed contract, reverts on error
     */
     function privateAddressOf(bytes32 _salt) external view returns (address) {
-        return this.addressOf(_salt, msg.sender);
+        return addressOf(_salt, msg.sender);
     }
 
     /**
@@ -40,8 +59,54 @@ contract Create3Factory {
            only -- note the salt is salted with `_sender` in order to allow for front-running resistant deployment
     @return addr of the deployed contract, reverts on error
     */
-    function addressOf(bytes32 _salt, address _sender) external view returns (address) {
+    function addressOf(bytes32 _salt, address _sender) public view returns (address) {
         _salt = keccak256(bytes.concat(_salt, bytes20(_sender)));
         return Create3.addressOf(_salt);
+    }
+
+    error UnsafeDeployRequiresOneWeiAtLeast();
+    /**
+    @notice Creates a new contract with given `_creationCode` and `_salt`, using the 0x00..00 address
+            in the salt to avoid collisions with `privateDeploy`, but using _this_ function is entirely
+            front-runable, so use it at your own peril! In order to make sure you understood this you
+            _must_ pay at least one wei more than `_salt` as symbolic token, though donations are welcome.
+    @param _salt Salt of the contract creation, resulting address will be derived from this value
+           only -- note the salt is salted with `bytes20(0)` in order to prevent front-running a `privateDeploy` call.
+    @param _creationCode Creation code (constructor) of the contract to be deployed, this value doesn't affect the resulting address
+    @return addr of the deployed contract, reverts on error
+    */
+   function unsafeDeploy(bytes32 _salt, bytes calldata _creationCode) external payable returns (address) {
+        if(msg.value <  uint256(_salt))
+            revert UnsafeDeployRequiresOneWeiAtLeast();
+        address addr = _deploy(_salt, bytes20(0), _creationCode);
+        emit DeployedUnsafe(addr);
+        return addr;
+   }
+
+    /**
+    @notice Generates the address from `_salt`, using the 0x00..00 address in the salt to avoid front-running
+    @param _salt Salt of the contract creation, resulting address will be derived from this value
+            only -- note the salt is salted with 0x00..00 in order to protect `privateDeploy` from front-running
+            while this version is really unsafe against that!
+    @return addr of the deployed contract, reverts on error
+    */
+    function unsafeAddressOf(bytes32 _salt) external view returns (address) {
+        return addressOf(_salt, address(0));
+    }
+
+    // last resorts if someone sent e.g. an ERC-20 token or NFT...
+    function _call(address addr, bytes calldata _calldata) external returns (bool, bytes memory) {
+        require(msg.sender == zommuter);
+        (bool success, bytes memory result) = addr.call(_calldata);
+        emit EmergencyCall(addr, success, result);
+        return (success, result);
+    }
+
+    // yup, this can be used to `selfdestruct` this contract by accident or purpose
+    function _delegatecall(address addr, bytes calldata _calldata) external returns (bool, bytes memory) {
+        require(msg.sender == zommuter);
+        (bool success, bytes memory result) = addr.delegatecall(_calldata);
+        emit EmergencyDelegateCall(addr, success, result);
+        return (success, result);
     }
 }
